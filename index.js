58d334e679dff4f256ff90a7649bb17d4ae29144
Atomico592 <atomico592@gmail.com>
2023-06-20 23:32:38 +0600

    Added second block

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-let div = document.createElement('div');
-div.className = "alert";
-div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+const returnCards = () => {
+
+    const array = ["75", "1", "35", " ", "885", "40"]
+    
+    for (let i = 0; index < 6; index++) {
+        let div = document.createElement('div');
+        div.className = "div-" + index;
+        div.innerHTML = array[i];
+    }
+}
+
+
+
+
