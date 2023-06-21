ef3479a015ceea39567de986fc7a40c20b490057
Atomico592 <atomico592@gmail.com>
2023-06-21 14:42:45 +0600

    Added number cards and styles for them

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const returnCards = () => {
-
-    const array = ["75", "1", "35", " ", "885", "40"]
-    
-    for (let i = 0; index < 6; index++) {
+    const array = ["75", "1", "35", "", "885", "40"]
+    const bgColorsArray = ["#f28e37", '#fc73b0', '#8e3dcb', '#94c94d', '#94c94d', '#4db8ec']
+     let allDiv = document.createElement('div');
+     allDiv.classList.add('cards-block');
+    for (let i = 0; i < array.length; i++) {
         let div = document.createElement('div');
-        div.className = "div-" + index;
-        div.innerHTML = array[i];
+        div.className = "div-" + i;
+        div.innerText = array[i];
+        div.style.width = "32%"
+        div.style.backgroundColor = bgColorsArray[i]
+        allDiv.append(div)
     }
+    return allDiv
 }
+
+
+
 
 
 
