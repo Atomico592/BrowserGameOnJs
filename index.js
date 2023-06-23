c95b29d38a912df5b08bc3a4442ad3afef60dd68
Atomico592 <atomico592@gmail.com>
2023-06-23 23:07:39 +0600

    Refactoring logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+   
 
 
 const returnCards = () => {
@@ -7,11 +7,11 @@ const returnCards = () => {
      let allDiv = document.createElement('div');
      allDiv.classList.add('cards-block');
     for (let i = 0; i < array.length; i++) {
-        let div = document.createElement('div')
-        div.className = "div-" + i;
-        div.innerText = array[i];
-        div.style.backgroundColor = bgColorsArray[i]
-        allDiv.append(div)
+        let previewCards = document.createElement('div')
+        previewCards.className = "div-" + i;
+        previewCards.innerText = array[i];
+        previewCards.style.backgroundColor = bgColorsArray[i]
+        allDiv.append(previewCards)
     }
     return allDiv
 }
@@ -38,7 +38,7 @@ const clickBtn = (className) => {
 
 const countdown = () => {
     
-  let сounter = document.getElementById('counter')
+    document.getElementById('counter')
   let p = document.createElement('p')
   let count = 3;
     
@@ -57,12 +57,14 @@ const countdown = () => {
 }
 
 
+
+
 const getRandomCards = (lvl) => {
 
     const bgColorsArray = ["#f28e37", '#fc73b0', '#8e3dcb', '#94c94d', '#94c94d', '#4db8ec']
-    const animations = ["blink .4s ease-in infinite alternate", "scale .4s ease-in infinite alternate", "rotate .6s linear infinite"]
+    const animationsClass= ["blink","scale","rotate"]
    
-    let item = document.getElementsByClassName('game-field-container')[0]
+    let elem = document.getElementsByClassName('game-field-container')[0]
     let createDiv = document.createElement('div');
     let gameFieldDiv = document.querySelector('.game-field');
     let count = 6;
@@ -76,7 +78,7 @@ const getRandomCards = (lvl) => {
         [numbers[k], numbers[j]] = [numbers[j], numbers[k]];
     }
 
-    item.style.backgroundColor = bgColorsArray[lvl - 1];
+    elem.style.backgroundColor = bgColorsArray[lvl - 1];
     createDiv.className = "game-field"
     
          if (lvl >= 4 && lvl < 6) {
@@ -87,78 +89,101 @@ const getRandomCards = (lvl) => {
             count = 25
         } 
 
-        for (let i = 1; i <= count; i++) {
-            let getRandomNumber;
-            switch (lvl) {
-                case 1:
+    for (let i = 1; i <= count; i++) {
+        let getRandomNumber;
+        switch (lvl) {
+            case 1:
                 getRandomNumber = numbers[i]
                 break;
-                case 2 :
-                getRandomNumber = Math.floor(Math.random() * (100 - 10 + 1)) + 10 
+            case 2:
+                getRandomNumber = Math.floor(Math.random() * (100 - 10 + 1)) + 10
                 break;
-                case 3 :
-                    case 4:
-                case 5:
-                    case 6:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
                 getRandomNumber = Math.floor(Math.random() * (1000 - 100 + 1)) + 100
                 break;
-                case 7 :
-                    case 8:
-                        case 9:
+            case 7:
+            case 8:
+            case 9:
                 getRandomNumber = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000
                 break;
-            }
+        }
             
-             let div = document.createElement('div')
-             div.className = "gameCards"
-            if (lvl > 2) div.style.animation = `${animations[Math.floor(Math.random() * 3)]}`
-
+        let gameNumCards = document.createElement('div')
+        gameNumCards.className = "gameCards"
+        
+       
             switch (count) {
                 case 12:
                     createDiv.style.gridTemplateColumns = "repeat(4, 0fr)"
-                    div.style.height = "64px"
-                    div.style.width = "117px"
+                    gameNumCards.style.height = "64px"
+                    gameNumCards.style.width = "117px"
                     break;
                 case 16:
                     createDiv.style.gridTemplateColumns = "repeat(4, 0fr)"
                     createDiv.style.left = "98px"
-                    div.style.height = "45px"
-                    div.style.width = "100px"
-                    div.style.fontSize = "20px" 
+                    gameNumCards.style.height = "45px"
+                    gameNumCards.style.width = "100px"
+                    gameNumCards.style.fontSize = "20px"
                     break;
-                case 25:
-                    createDiv.style.gridTemplateColumns = "repeat(5, 0fr)"
-                    createDiv.style.gridColumnGap = "12px"
-                    createDiv.style.gridRowGap = "13px"
-                    createDiv.style.left = "80px"
-                    div.style.height = "37px"
-                    div.style.width = "89px"
-                    div.style.fontSize = "18px"
-                    break;
-                default:
-                    break;
-            } 
-            div.innerText = getRandomNumber
-            //  div.innerHTML = `<span>${getRandomNumber}</span>`;
-            //  div.firstChild.style.animation = "rotate .6s linear infinite"
-             div.style.backgroundColor = bgColorsArray[Math.floor(Math.random() * (5 - 0 + 1)) + 0]
-             createDiv.append(div)
-         } 
-         return createDiv
+                    case 25:
+                        createDiv.style.gridTemplateColumns = "repeat(5, 0fr)"
+                        createDiv.style.gridColumnGap = "12px"
+                        createDiv.style.gridRowGap = "13px"
+                        createDiv.style.left = "80px"
+                        gameNumCards.style.height = "37px"
+                        gameNumCards.style.width = "89px"
+                        gameNumCards.style.fontSize = "18px"
+                        break;
+                        default:
+                            break;
+        } 
+
+        
+                          if (lvl > 2) {
+                              let randomAnimation = Math.floor(Math.random() * 3)
+                              if (randomAnimation === 2) {
+                                  gameNumCards.innerHTML = `<span class='inner-span'>${getRandomNumber}</span>`
+                                  let a = document.getElementsByClassName('inner-span')
+                                  a.classList.add(animationsClass[2])
+                              } else {
+                                  gameNumCards.classList.add(`${animationsClass[Math.floor(Math.random() * 2)]}`)
+                              }  
+                             }
+                    
+                      
+                        gameNumCards.innerHTML = `<span class='inner-span'>${getRandomNumber}</span>`
+                        gameNumCards.style.backgroundColor = bgColorsArray[Math.floor(Math.random() * (5 - 0 + 1)) + 0]
+                        createDiv.append(gameNumCards)
+                    } 
+                    return createDiv
 }
 
 
 const findTheNumber = () => {
-    let gameField = document.querySelector('.game-field').childNodes  
-    let item = Math.floor(Math.random() * gameField.length)
-    let getNum = gameField[item].innerText
+    let currentCard = document.querySelectorAll(".gameCards")
+    let random = Math.floor(Math.random() * currentCard.length)
+    let getNum = currentCard[random].innerText
     document.querySelector('#toFindWhat').innerText = getNum
-        
     document.addEventListener('click', (e) => {
-        if (gameState.currentLvl <= gameState.gameLvl - 1)
-        if (e.target.innerText === getNum) {
-            gameState.currentLvl = gameState.currentLvl + 1
-            letsPlay(gameState.currentLvl)
+        if (document.querySelector('.third-page').style.display === "none") {
+            if (gameState.currentLvl <= gameState.gameLvl - 1) {
+                if (e.target.innerText === getNum) {
+                    gameState.currentLvl++
+                    if (gameState.bonus !==5) gameState.bonus++
+                    gameState.currentUnswer++
+                    gameState.points += (Math.floor(Math.random() * (220 - 20 + 1)) + 20) * gameState.bonus
+                        letsPlay(gameState.currentLvl)
+                    
+                } else {
+                    // gameState.currentUnswer--
+                    // gameState.bonus--
+                }
+            } else {
+                document.querySelector('.game-page').style.display = "none"
+            }
         }
     })
     
@@ -166,14 +191,43 @@ const findTheNumber = () => {
 
 
 
-  const letsPlay = (num) => {
-        document.getElementById("lvl").innerText = `${gameState.currentLvl} - ${gameState.gameLvl}`
-        document.getElementById("points").innerText = gameState.points
-        document.getElementById("bonus").innerText = gameState.bonus
+const letsPlay = (num) => {
+    document.getElementById("lvl").innerText = `${gameState.currentLvl} - ${gameState.gameLvl}`
+    document.getElementById("points").innerText = gameState.points
+    document.getElementById("bonus").innerText = `X${gameState.bonus}`
+    document.querySelector('#result-page-content-point').innerHTML = gameState.points
+    document.querySelector('#result-page-content-answers').innerHTML = `${gameState.currentUnswer} из ${gameState.gameLvl}`
+    document.querySelector('#result-page-content-accuracy').innerHTML = `${gameState.incorrectUnswer === 0 ? "100%" : `${(gameState.currentUnswer * 100) / gameState.incorrectUnswer}%`}`
       let item = document.getElementsByClassName('game-field-container')[0]
       item.append(getRandomCards(num))
-        findTheNumber()
-  }
+      findTheNumber()    
+}
+    
+
+
+         const startTimer = () => {
+             gameState.timer--;
+             document.querySelector('.game-field-menu-title-inner-text').innerText = `00:${gameState.timer < 10 ? "0" + gameState.timer : gameState.timer}`
+             if (gameState.timer === 0) {
+                 clearInterval(timerInterval);
+             }
+         }
+
+         let timerInterval = setInterval(startTimer, 1000)
+    
+
+
+
+
+
+
+
+   
+    
+ 
+  
+
+
 
 
 
